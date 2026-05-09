ba3a1bad05f4bd6799763f85ddbdcfce7ad59009
Ruslan <rusik07103@gmail.com>
2026-05-09 10:07:47 +0300

    next

--- sprint1/problems/cafeteria/solution/src/cafeteria.h
+++ sprint1/problems/cafeteria/solution/src/cafeteria.h
@@ -115,9 +115,8 @@ public:
 
 private:
     net::io_context& io_;
-	std::atomic_int next_id_ = 0; 
+    std::atomic_int next_hotdog_id_ = 0;
     Store store_;
 
-    std::shared_ptr<GasCooker> gas_cooker_ =
-        std::make_shared<GasCooker>(io_);
+    std::shared_ptr<GasCooker> gas_cooker_ = std::make_shared<GasCooker>(io_);
 };
